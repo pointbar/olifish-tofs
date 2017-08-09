19337366405d1edeeeb7688791e452b2dd229aec
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-09 19:26:28 +0200

    Test data

--- export-meta/push-data.sh
+++ export-meta/push-data.sh
@@ -4,6 +4,7 @@ mkdir olifish
 cd olifish
 git clone https://c8ac96de423f60f22af74ce6b74d83d4ecb1b83c@github.com/pointbar/olifish.git .
 node ../export-meta/meta.js
+cat data.js
 git add .
 git commit -m "Update date"
 git push origin master
