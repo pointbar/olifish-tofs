1c4e4a29c63d4364041d86584595e689cf539ac4
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-09 19:37:20 +0200

    Test data

--- export-meta/push-data.sh
+++ export-meta/push-data.sh
@@ -2,7 +2,7 @@
 
 mkdir olifish
 cd olifish
-git clone https://c8ac96de423f60f22af74ce6b74d83d4ecb1b83c@github.com/pointbar/olifish.git .
+git clone https://$token@github.com/pointbar/olifish.git .
 node ../export-meta/meta.js
 cat data.js
 git add .
