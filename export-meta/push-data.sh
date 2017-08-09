797e563a5a929deaa622a191b7ea9f5f0b6464db
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-09 20:51:47 +0200

    travis

--- export-meta/push-data.sh
+++ export-meta/push-data.sh
@@ -3,4 +3,4 @@
 node export-meta/meta.js
 git add .
 git commit -m "Update data.js"
-git push "https://${token}@github.com/olivier-colli/olifish-tofs.git"
+git push https://$token@github.com/olivier-colli/olifish-tofs.git
