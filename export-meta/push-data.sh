dfc8ef30c439e456cfe9324d4ae8a6e953c8d388
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-11 23:55:57 +0200

    remove exif node module in commit

--- export-meta/push-data.sh
+++ export-meta/push-data.sh
@@ -2,6 +2,6 @@
 npm install --save-dev node-exiftool
 node export-meta/meta.js
 git checkout master
-git add .
+git add data.js
 git commit -m "Update data.js"
 git push https://$token@github.com/olivier-colli/olifish-tofs.git
