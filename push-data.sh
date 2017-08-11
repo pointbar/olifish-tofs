ae4a2bd114c83859c88ca3f9b8bb6cebc1440693
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-12 01:06:19 +0200

    final

--- push-data.sh
+++ push-data.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 git checkout master
 node meta.js
-cat data.json
-git status
 git add data.json
 git commit -m "Update data.json"
 git push https://$token@github.com/olivier-colli/olifish-tofs.git
