257098db1c151c50ae516d73bd136c86c90c8066
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-11 23:40:11 +0200

    update

--- export-meta/meta.js
+++ export-meta/meta.js
@@ -1,19 +1,24 @@
 const fs = require('fs')
+const exiftool = require('node-exiftool')
 
-const folder = 'img'
-
-const readThumbs = (folder, extract) => {
-    const metas = []
-    fs.readdir(folder, (err, files) => {
-        const thumbs = files.filter(file => file.match(/^thumb-/))
-        thumbs.map(thumb => extract(metas, { fileName: thumb }))
-        fs.writeFile('data.js', JSON.stringify(metas), 'utf8', err => {
+const ep = new exiftool.ExiftoolProcess()
+ep.open()
+    // read directory
+    .then(() => ep.readMetadata('img-en-attente', ['-File:all']))
+    .then((metas, err) => {
+        const metasClean = []
+        metas.data.forEach(meta => {
+            metasClean.push({
+                dateCreated: meta.dateCreated || new Date('01/01/2010'),
+                location: meta['Caption-Abstract'] || '',
+                keywords: meta.keywords || [],
+                model: meta.Model || []
+            })
+        })
+        fs.writeFile('data.js', `const data = ${JSON.stringify(metasClean)}`, 'utf8', err => {
             if (err) throw err
             console.log('The file has been saved!')
         })
     })
-}
-
-const addMeta = (metas, meta) => metas.push(meta)
-
-readThumbs(folder, addMeta)
+    .then(() => ep.close())
+    .catch(console.error)
