66ef35b4056c129bbc84faa4a5dddce6a30d70d7
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-12 02:12:39 +0200

    fin de journée

--- meta.js
+++ meta.js
@@ -15,13 +15,28 @@ const fs = require('fs')
 const exiftool = require('node-exiftool')
 
 const ep = new exiftool.ExiftoolProcess()
+
+const extractFieldsFromKeywords = (keywords) => {
+    const fields = {}
+    fields.keyword = []
+    keywords.forEach(keyword => {
+        if (!keyword.match(/\w*:/)) {
+            fields.keyword.push(keyword)
+        } else {
+            let field = keyword.split(':')
+            fields[field[0]] = decodeURIComponent(escape(field[1].trim()))
+        }
+    })
+    return fields
+}
+
 ep.open()
-    // read directory
     .then(() => ep.readMetadata('./img-en-attente', ['-File:all']))
     .then((metas, err) => {
+        console.log('metas', metas)
         const metasClean = []
         metas.data.forEach(meta => {
-            console.log('meta', meta);
+            console.log(meta)
             const fields = {}
             fields.dateCreated = meta.dateCreated || new Date('01/01/2010')
             fields.location = meta['Caption-Abstract'] || ''
@@ -36,4 +51,3 @@ ep.open()
     })
     .then(() => ep.close())
     .catch(console.error)
-console.log("End")
